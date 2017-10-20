4b4b891ee36e83553dc89a86b643b0a742ea00c2
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2017-10-20 14:43:07 +0700

    change error of image (3.5)

--- src/main/java/camt/cbsd/lab05/controller/StudentController.java
+++ src/main/java/camt/cbsd/lab05/controller/StudentController.java
@@ -3,24 +3,41 @@ package camt.cbsd.lab05.controller;
 import camt.cbsd.lab05.entity.Student;
 import camt.cbsd.lab05.service.StudentService;
 import camt.cbsd.lab05.service.StudentServiceImpl;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.websocket.server.PathParam;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalTime;
 import java.util.List;
 
+@ConfigurationProperties(prefix = "server")
 @RestController
 public class StudentController {
     StudentService studentService;
+    String  imageServerDir;
+
+    public String getImageServerDir() {
+        return imageServerDir;
+    }
+
+    public void setImageServerDir(String imageServerDir) {
+        this.imageServerDir = imageServerDir;
+    }
+
     @Autowired
     public void setStudentService(StudentService studentService) {
         this.studentService = studentService;
@@ -55,9 +72,36 @@ public class StudentController {
             produces = {MediaType.IMAGE_GIF_VALUE,
             MediaType.IMAGE_JPEG_VALUE,MediaType.IMAGE_PNG_VALUE})
     public @ResponseBody ResponseEntity<?> getStudentImage(@PathVariable("fileName")String fileName) throws IOException{
-        File file = Paths.get("C:/Works/Component-Based Software Dev/Final/Lab/images/lab07/a.jpg").toFile();
-        InputStream in = new FileInputStream(file);
-        return ResponseEntity.ok(IOUtils.toByteArray(in));
+        File file = Paths.get(imageServerDir+fileName).toFile();
+        if(file.exists()){
+            InputStream in = new FileInputStream(file);
+            return ResponseEntity.ok(IOUtils.toByteArray(in));
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+
+    }
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadImage(@RequestParam("file")MultipartFile file){
+        if(file.isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        try {
+            byte[] bytes = file.getBytes();
+            //Path path = Paths.get(imageServerDir+file.getOriginalFilename());
+            String oldFilename = file.getOriginalFilename();
+            String ext = FilenameUtils.getExtension(oldFilename);
+            String newFilename = Integer.toString(LocalTime.now().hashCode(),16)
+                    +Integer.toString(oldFilename.hashCode(),16)+"."+ext;
+            Path path = Paths.get(imageServerDir+newFilename);
+            Files.write(path,bytes);
+            //return ResponseEntity.ok(file.getOriginalFilename());
+            return ResponseEntity.ok(newFilename);
+        }catch (IOException e ){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
 }
